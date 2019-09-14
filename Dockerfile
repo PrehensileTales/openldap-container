be8da6c32819feac6947035bfcb07ff80cdfde86
Hein-Pieter van Braam-Stewart <hp@tmm.cx>
2019-09-14 15:21:31 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM fedora:30
+
+LABEL maintainer="Hein-Pieter van Braam-Stewart <hp@prehensile-tales.com>"
+
+VOLUME /data
+EXPOSE 389/tcp
+EXPOSE 636/tcp
+
+RUN dnf -y install openldap-servers openldap-clients pwgen && \
+    dnf clean all && \
+    mkdir -p /data && \
+    chown ldap.ldap /data
+
+COPY startslapd.sh /usr/local/sbin
+RUN chmod +x /usr/local/sbin/startslapd.sh
+
+ENV INITIAL_SCHEMAS="core cosine inetorgperson"
+
+ENV INITIAL_SUFFIX_DN_1="dc=example,dc=com"
+ENV INITIAL_ORGANIZATION_1="Example organization"
+
+ENV INITIAL_ROOTPW=""
+
+ENV SLAPD_LISTEN_LDAPS=1
+ENV SLAPD_LISTEN_LDAP=1
+ENV SLAPD_LISTEN_LDAPI=1
+ENV SLAPD_LOGLEVEL=2
+
+CMD /usr/local/sbin/startslapd.sh
---
+++ startslapd.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ ! -e /data/conf.d ]; then
+if [ -z "${INITIAL_ROOTPW}" ]; then
+  rootpw=$(pwgen 10)
+else
+  rootpw="${INITIAL_ROOTPW}"
+fi
+
+  initial_suffixes=""
+  suffix_variable_suffixes="$(for var in $(env | grep ^INITIAL_SUFFIX_DN | cut -d "=" -f 1); do echo ${var##*_}; done)"
+  if [ ! -z "${suffix_variable_suffixes}" ]; then
+    for suffix_variable_suffix in ${suffix_variable_suffixes}; do
+      org="INITIAL_ORGANIZATION_${suffix_variable_suffix}"
+      suffix="INITIAL_SUFFIX_DN_${suffix_variable_suffix}" 
+      if [ -z "${!org}" ]; then
+        echo "ERROR: Suffix ${!suffix} has no organization name set"
+        exit 1
+      fi
+      initial_suffixes="${initial_suffixes} ${!suffix}"
+    done
+  fi
+
+  mkdir -p /data/conf.d
+  mkdir -p /data/databases
+
+/usr/sbin/slapadd -F /data/conf.d -n0 <<EOF
+dn: cn=config
+objectClass: olcGlobal
+cn: config
+
+dn: cn=module,cn=config
+objectClass: olcModuleList
+cn: module
+olcModulepath: /usr/lib64/openldap
+
+dn: cn=schema,cn=config
+objectClass: olcSchemaConfig
+cn: schema
+
+$(for schema in ${INITIAL_SCHEMAS}; do echo include: file:///etc/openldap/schema/${schema}.ldif; done)
+
+dn: olcDatabase=config,cn=config
+objectClass: olcDatabaseConfig
+olcDatabase: config
+olcAccess: to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" manage by * none
+olcRootDN: cn=manager,cn=config
+olcRootPW: $(/usr/sbin/slappasswd -s "${rootpw}")
+
+dn: olcDatabase=monitor,cn=config
+objectClass: olcDatabaseConfig
+olcDatabase: monitor
+olcAccess: to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" read by * none
+
+$(for suffix in ${initial_suffixes}; do 
+  mkdir -p "/data/databases/${suffix}"
+  echo "dn: olcDatabase=mdb,cn=config
+objectClass: olcDatabaseConfig
+objectClass: olcMdbConfig
+olcDatabase: mdb
+olcSuffix: ${suffix}
+olcRootDN: cn=Manager,${suffix}
+olcDbDirectory:	/data/databases/${suffix}
+olcDbIndex: objectClass eq,pres
+olcDbIndex: ou,cn,mail,surname,givenname eq,pres,sub
+"; done)
+EOF
+
+  for suffix_variable_suffix in ${suffix_variable_suffixes}; do
+    org="INITIAL_ORGANIZATION_${suffix_variable_suffix}"
+    suffix="INITIAL_SUFFIX_DN_${suffix_variable_suffix}"
+    /usr/sbin/slapadd -F /data/conf.d -b ${!suffix} <<EOF
+dn: ${!suffix}
+objectClass: dcObject
+objectClass: organization
+dc: $(echo ${!suffix} | sed -e 's/dc=\([^,]\+\).*/\1/')
+o: ${!org}
+EOF
+  done
+
+  chown -R ldap.ldap /data/
+
+  echo "NOTE: cn=manager,cn=config password is ${rootpw}"
+  echo "NOTE: this message will not be repeated!"
+fi
+
+urls=""
+if [ ${SLAPD_LISTEN_LDAP} -eq 1 ]; then urls="$urls ldap://"; fi
+if [ ${SLAPD_LISTEN_LDAPS} -eq 1 ]; then urls="$urls ldaps://"; fi
+if [ ${SLAPD_LISTEN_LDAPI} -eq 1 ]; then urls="$urls ldapi://%2Fdata%2Fldapi/"; fi
+
+/usr/sbin/slapd -h "${urls}" -u ldap -d${SLAPD_LOGLEVEL} -F /data/conf.d
